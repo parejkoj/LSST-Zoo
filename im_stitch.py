237e02ca1e80bd9c7ab493d3069f79261854047e
dougbrn <dougrbranton@gmail.com>
2016-12-19 14:29:59 -0800

    Real set

--- im_stitch.py
+++ im_stitch.py
@@ -16,7 +16,7 @@ cutouts=data["cutouts"]
 Q=5
 
 for i in range(len(cutouts)):
-    if not np.isnan(np.sum(cutouts[i][0])):
+    if not np.isnan(np.sum(cutouts[i][0])) and cutouts[i][3]!=0:
         template=cutouts[i][0]
         science=cutouts[i][1]
         difference=cutouts[i][2]
@@ -26,20 +26,25 @@ for i in range(len(cutouts)):
         Q=5
         scaled=luptonRGB.makeRGB(stitched_array,Q=Q,minimum=minimum)
         template=scaled[:,:20,0]
-        science=scaled[:,21:41,0]
+        science=scaled[:,20:40,0]
         difference=scaled[:,40:,0]
+        cenx=9.5
+        ceny=9.5
 
         f, (ax1, ax2, ax3) = plt.subplots(1, 3)
 
         ax1.imshow(template,cmap=cm.viridis,interpolation="none")
         ax1.axis('off')
+        ax1.scatter(cenx, ceny, marker='+', color='r', s=100)
         ax2.imshow(science,cmap=cm.viridis,interpolation="none")
         ax2.axis('off')
+        ax2.scatter(cenx, ceny, marker='+', color='r', s=100)
         ax3.imshow(difference,cmap=cm.viridis,interpolation="none")
         ax3.axis('off')
+        ax3.scatter(cenx, ceny, marker='+', color='r', s=100)
         ax1.set_title("Template")
         ax2.set_title("Science")
         ax3.set_title("Difference")
-        plt.tight_layout(w_pad=-1.5)
+        plt.tight_layout(w_pad=-3)
         plt.savefig('sub_sets/Asteroids/ast'+str(i)+'.jpg',bbox_inches='tight')
         plt.close()
