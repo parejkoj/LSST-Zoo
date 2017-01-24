e6536554bbd3a4ddf6cb9e63fba44db187318499
dougbrn <dougrbranton@gmail.com>
2017-01-24 13:55:07 -0800

    1x4 plotting

--- make_tracklet_coadd_cutouts.py
+++ make_tracklet_coadd_cutouts.py
@@ -188,7 +188,10 @@ if __name__ == "__main__":
 
                 for cutout_n, (cutout, cutout_data, sci_cutouts) in enumerate(cutout_group):
                     count=0
+                    print cutout_n
+                    print sci_cutouts
                     for sci_cutout in sci_cutouts:
+                        print "made it to loop"
                         count+=1
                         top_level_grid=gridspec.GridSpec(1,4)
 
@@ -216,6 +219,8 @@ if __name__ == "__main__":
                         plt.title("Coadd", color='white')
                         plt.axis("off")
 
+                        print "made it to savefig"
+
                         plt.subplots_adjust(hspace=0.02, wspace=0.02, left=0.05, right=0.95,
                                     top=0.95, bottom=0.05)
                         plt.savefig("cutouts/cutouts_tract{:d}_p{:d}{:d}_{:0d}_{:d}.png".format(tract_info.getId(),
@@ -223,3 +228,4 @@ if __name__ == "__main__":
                                                                            target_patch.getIndex()[1],
                                                                            group_n,str(count)),
                                                                            dpi=150, facecolor='k')
+                        print "past savefig"
